8c55969598329b2f4b8822fea351c7b7b0a4bcee
Mark Beaton <mark@markbeaton.com>
2010-06-10 21:49:45 +1000

    Initial import

---
+++ AssemblyInfo.cs
@@ -0,0 +1,55 @@
+// Copyright (c) 2009 Mark Beaton
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TidyManaged")]
+[assembly: AssemblyDescription("Managed .NET wrapper for the HTML Tidy library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mark Beaton")]
+[assembly: AssemblyProduct("TidyManaged")]
+[assembly: AssemblyCopyright("Copyright © Mark Beaton 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c09c222-dbe1-44b3-8983-d4116ec3e051")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Interop/PInvoke.cs
@@ -0,0 +1,84 @@
+// Copyright (c) 2009 Mark Beaton
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace TidyManaged.Interop
+{
+	internal class PInvoke
+	{
+		[DllImport("libtidy.dll")]
+		internal static extern IntPtr tidyCreate();
+
+		[DllImport("libtidy.dll")]
+		internal static extern void tidyRelease(IntPtr tdoc);
+
+		[DllImport("libtidy.dll")]
+		internal static extern IntPtr tidyReleaseDate();
+
+		[DllImport("libtidy.dll")]
+		internal static extern IntPtr tidyOptGetValue(IntPtr tdoc, TidyOptionId optId);
+
+		[DllImport("libtidy.dll")]
+		internal static extern bool tidyOptSetValue(IntPtr tdoc, TidyOptionId optId, string val);
+
+		[DllImport("libtidy.dll")]
+		internal static extern uint tidyOptGetInt(IntPtr tdoc, TidyOptionId optId);
+
+		[DllImport("libtidy.dll")]
+		internal static extern bool tidyOptSetInt(IntPtr tdoc, TidyOptionId optId, uint val);
+
+		[DllImport("libtidy.dll")]
+		internal static extern bool tidyOptGetBool(IntPtr tdoc, TidyOptionId optId);
+
+		[DllImport("libtidy.dll")]
+		internal static extern bool tidyOptSetBool(IntPtr tdoc, TidyOptionId optId, bool val);
+
+		[DllImport("libtidy.dll")]
+		internal static extern int tidyParseFile(IntPtr tdoc, string filename);
+
+		[DllImport("libtidy.dll")]
+		internal static extern int tidyParseString(IntPtr tdoc, IntPtr content);
+
+		[DllImport("libtidy.dll")]
+		internal static extern int tidyParseSource(IntPtr tdoc, ref TidyInputSource source);
+
+		[DllImport("libtidy.dll")]
+		internal static extern int tidyCleanAndRepair(IntPtr tdoc);
+
+		[DllImport("libtidy.dll")]
+		internal static extern int tidySaveFile(IntPtr tdoc, string filnam);
+
+		[DllImport("libtidy.dll")]
+		internal static extern int tidySaveString(IntPtr tdoc, IntPtr buffer, ref uint buflen);
+
+		[DllImport("libtidy.dll")]
+		internal static extern int tidySaveSink(IntPtr tdoc, ref TidyOutputSink sink);
+
+		internal static string tidyOptGetValueString(IntPtr tdoc, TidyOptionId optId)
+		{
+			return Marshal.PtrToStringAnsi(tidyOptGetValue(tdoc, optId));
+		}
+	}
+}
